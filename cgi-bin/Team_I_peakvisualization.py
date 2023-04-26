3e48d5872ea14f77d54ea09b8bb8343ab8f090b5
mkpannu <mkpannu1221@gmail.com>
2023-04-25 22:53:58 -0400

    Peak Visualization update

--- cgi-bin/Team_I_peakvisualization.py
+++ cgi-bin/Team_I_peakvisualization.py
@@ -26,18 +26,19 @@ if form:
     selector = form.getvalue("selector","")
     timepoint = form.getvalue("timepoint", 0)
     chromosome = form.getvalue("chromosome", "")
+    cell_line = form.getvalue("cell_line", "")
     
     if (selector == "plotpeaks"):
 
         query1 = """
         SELECT abs_summit, signalValue
         FROM PEAK JOIN SAMPLE USING(sid)
-        WHERE timepoint = %s AND cid = %s
+        WHERE timepoint = %s AND cid = %s AND cell_line = %s
         ORDER BY abs_summit
         """
 
         try: 
-            cursor.execute(query1, [int(timepoint), chromosome])
+            cursor.execute(query1, [int(timepoint), chromosome, cell_line])
         except pymysql.Error as e: 
             print(e,query1)
         
--- cgi-bin/Team_I_search.py
+++ cgi-bin/Team_I_search.py
@@ -28,18 +28,19 @@ if form:
     chromosome = form.getvalue("chromosome", "")
     start_pos = form.getvalue("start_pos", 0)
     stop_pos = form.getvalue("stop_pos", 0)
+    cell_line = form.getvalue("cell_line", "")
     
     if (selector == "summarytable"):
 
         query1 = """
         SELECT cell_line, replicate, start, end, abs_summit, fold_enrichment, pvalue
         FROM PEAK JOIN SAMPLE USING (sid) 
-        WHERE timepoint = %s AND cid= %s AND start > %s AND end < %s
+        WHERE timepoint = %s AND cid= %s AND start > %s AND end < %s AND cell_line = %s
         ORDER BY start;
         """
 
         try: 
-            cursor.execute(query1, [int(timepoint), chromosome, int(start_pos), int(stop_pos)])
+            cursor.execute(query1, [int(timepoint), chromosome, int(start_pos), int(stop_pos), cell_line])
         except pymysql.Error as e: 
             print(e,query1)
         
